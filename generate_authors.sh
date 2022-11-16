036771e37b7b29de6272b0e126ef7e608620f8a7
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-11-16 07:21:00 +0000

    Refactor

--- generate_authors.sh
+++ generate_authors.sh
@@ -3,7 +3,7 @@
 #https://github.com/docker/docker/blob/master/hack/generate-authors.sh
 set -e
 
-#cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
 # see also ".mailmap" for how email addresses and names are deduplicated
 
